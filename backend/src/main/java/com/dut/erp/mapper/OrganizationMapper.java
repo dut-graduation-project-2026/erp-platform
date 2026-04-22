671e68827227106d845b6a00cd8df0dcf396160a
halwins <minhduc.fwrks@gmail.com>
2026-04-22 16:03:42 +0700

    refactor: reorder import statements and adjust method indentation in OrganizationMapper

--- backend/src/main/java/com/dut/erp/mapper/OrganizationMapper.java
+++ backend/src/main/java/com/dut/erp/mapper/OrganizationMapper.java
@@ -1,16 +1,15 @@
 package com.dut.erp.mapper;
 
+import com.dut.erp.dto.response.OrganizationBaseResponse;
+import com.dut.erp.entity.Organization;
 import org.mapstruct.Mapper;
 import org.mapstruct.NullValueCheckStrategy;
 import org.mapstruct.NullValuePropertyMappingStrategy;
-
-import com.dut.erp.dto.response.OrganizationBaseResponse;
-import com.dut.erp.entity.Organization;
 
 @Mapper(
     componentModel = "spring",
     nullValueCheckStrategy = NullValueCheckStrategy.ALWAYS,
     nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
 public interface OrganizationMapper {
-    OrganizationBaseResponse toOrganizationBaseResponse(Organization entity);
+  OrganizationBaseResponse toOrganizationBaseResponse(Organization entity);
 }
