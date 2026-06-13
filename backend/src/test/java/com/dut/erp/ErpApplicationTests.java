30f56a434152726736fb95eecbdbb7e9cf0dd3ca
halwins <minhduc.fwrks@gmail.com>
2026-06-14 00:23:23 +0700

    feat: add Testcontainers integration test infrastructure with PostgreSQL container support

--- backend/src/test/java/com/dut/erp/ErpApplicationTests.java
+++ backend/src/test/java/com/dut/erp/ErpApplicationTests.java
@@ -1,10 +1,8 @@
 package com.dut.erp;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class ErpApplicationTests {
+class ErpApplicationTests extends AbstractIntegrationTest {
 
   @Test
   void contextLoads() {}
